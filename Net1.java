a000efb47d52e8a906e8abd62bd5b8ee6de3e1bd
Bryant <101554583+bryant440@users.noreply.github.com>
2023-03-16 18:52:27 +0300

    Update Net1.java
    test;try& catch

--- Net1.java
+++ Net1.java
@@ -10,10 +10,18 @@ public class Net1 {
             System.out.println("Port : " + url.getPort());
             System.out.println("Filename : " + url.getFile());
             System.out.println("Ref : " + url.getRef());
-            System.out.println("Parh: " + url.getPath());
+            System.out.println("Path: " + url.getPath());
         } catch (Exception e) {
             // TODO: handle exception
             e.printStackTrace();
         }
     }
 }
+/*
+url.getHost() returns the host name of the URL ("blog.openreplay.com" in this case).
+url.getProtocol() returns the protocol of the URL ("https" in this case).
+url.getPort() returns the port number of the URL (-1 because it is not specified in this case).
+url.getFile() returns the file name of the URL ("/an-introduction-to-babel/" in this case).
+url.getRef() returns the reference of the URL (null because it is not specified in this case).
+url.getPath() returns the path of the URL ("/an-introduction-to-babel/" in this case).
+*/
