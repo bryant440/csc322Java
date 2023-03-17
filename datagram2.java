cbe9167a0223fabc1b86afaf1e82119e8bce255d
Bryant <101554583+bryant440@users.noreply.github.com>
2023-03-17 10:22:01 +0300

    Create datagram2.java
    test:datagram server side

---
+++ datagram2.java
@@ -0,0 +1,10 @@
+public class UDPServer{
+public static void main(String[] args) throws IOException{
+DatagramSocket server = new DatagramSocket(4120);
+byte [] buf = new byte[256];
+DatagramPacket packet = new DatagramPacket(buf,buf.length);
+ server.receive(packet);
+  String response = new String(packet.getData());
+  System.out.println("response" + response)
+}
+}
