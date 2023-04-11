84d6171c0c2121edeecf60734e50f6a8318eb451
Bryant <101554583+bryant440@users.noreply.github.com>
2023-04-11 12:02:18 +0300

    Update datagram.java
    .

--- datagram.java
+++ datagram.java
@@ -3,9 +3,9 @@ public class UDPClient{
 public static void main(String[] args){
 //
 DatagramSocket client = new DatagramSocket();
-InetAddress add = InetAddress.getByName("local host");
-String guap = "miss the rage";
-  byte [] buf = guap.getBytes();
+InetAddress add = InetAddress.getByName("localhost");
+String meg = "miss the rage";
+  byte [] buf = meg.getBytes();
   DatagramPacket p = new DatagramPacket(buf,buf.length, add , 4160);
   client.send(p);
 }
